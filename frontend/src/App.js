777f8bc3f999905d4cec92fa08808dcfe7d77ffc
CornejoFrancisco <francisco.cornejo1978@gmail.com>
2024-06-02 12:00:53 -0300

    Agrege una nueva funcional que permite a los usuario poner me gusta en los articulos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from '.src/Components/Header';
+import ProductGrid from './Components/ProductGrid';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <ProductGrid />
     </div>
   );
 }
